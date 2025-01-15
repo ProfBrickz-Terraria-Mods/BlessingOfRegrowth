9b8150961645f9158aa4bd9a09a5e8f969f90b01
ProfBrickz <79584392+ProfBrickz@users.noreply.github.com>
2025-01-15 13:48:12 -0500

    fix saving player data

--- Common/Player.cs
+++ Common/Player.cs
@@ -1,8 +1,17 @@
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 
 namespace BlessingOfRegrowth.Common {
    public class BlessingOfRegrowthPlayer : ModPlayer {
       public bool unlockedRegrowthMode = false;
+
+      public override void SaveData(TagCompound tag) {
+         tag["unlockedRegrowthMode"] = unlockedRegrowthMode;
+      }
+
+      public override void LoadData(TagCompound tag) {
+         unlockedRegrowthMode = tag.GetBool("unlockedRegrowthMode");
+      }
    }
 }
