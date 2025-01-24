549be6bfcf38066ae5a4e777ff532b4a1b4a3393
ProfBrickz <79584392+ProfBrickz@users.noreply.github.com>
2025-01-24 13:18:57 -0500

    Add support for Regrowth Replant Mod
    fix #7

--- BlessingOfRegrowth.cs
+++ BlessingOfRegrowth.cs
@@ -1,67 +1,262 @@
-// Imports
 using System;
+using System.Reflection;
 using BlessingOfRegrowth.Common;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-
+using Terraria.ObjectData;
 
 namespace BlessingOfRegrowth {
    public class BlessingOfRegrowth : Mod {
-      // Timer to disable sapling breaking
-      private DateTime saplingBreakDisableTimer = DateTime.Now;
-      // Time duration to disable sapling breaking in seconds
-      private const int saplingBreakDisableTime = 5;
+      private DateTime plantBreakDisableTimer = DateTime.Now;
+      private const int plantBreakDisableTime = 5;
+      private Mod regrowthReplant = null;
+
+      ushort[] gemcornTrees = {
+         TileID.TreeTopaz,
+         TileID.TreeAmethyst,
+         TileID.TreeSapphire,
+         TileID.TreeEmerald,
+         TileID.TreeRuby,
+         TileID.TreeDiamond,
+         TileID.TreeAmber
+      };
 
       public override void Load() {
          // Hook into the mining tool usage event
          On_Player.ItemCheck_UseMiningTools_ActuallyUseMiningTool += BreakTile;
       }
 
-      private void BreakTile(On_Player.orig_ItemCheck_UseMiningTools_ActuallyUseMiningTool orig, Player player, Item sItem, out bool canHitWalls, int treeX, int treeY) {
+      private void BreakTile(
+         On_Player.orig_ItemCheck_UseMiningTools_ActuallyUseMiningTool orig,
+         Player player,
+         Item sItem,
+         out bool canHitWalls,
+         int x,
+         int y
+      ) {
          // Check if the player has unlocked and is using Regrowth Mode, and is not holding the Axe of Regrowth
          if (
             !player.GetModPlayer<BlessingOfRegrowthPlayer>().unlockedRegrowthMode ||
             !player.GetModPlayer<BlessingOfRegrowthPlayer>().usingRegrowthMode ||
             player.HeldItem.type == ItemID.AcornAxe
          ) {
-            orig.Invoke(player, sItem, out canHitWalls, treeX, treeY);
+            orig.Invoke(player, sItem, out canHitWalls, x, y);
             return;
          }
+
+         if (ModLoader.HasMod("RegrowthReplant")) {
+            regrowthReplant = ModLoader.GetMod("RegrowthReplant");
+
+            bool isHerb = BreakHerb(orig, player, sItem, out canHitWalls, x, y);
+            if (isHerb) return;
+
+            bool isGemcorn = BreakGemcorn(orig, player, sItem, out canHitWalls, x, y);
+            if (isGemcorn) return;
+         }
+
+         bool isTree = BreakTree(orig, player, sItem, out canHitWalls, x, y);
+         if (isTree) return;
+
+         orig.Invoke(player, sItem, out canHitWalls, x, y);
+      }
+
+      private bool BreakTree(
+         On_Player.orig_ItemCheck_UseMiningTools_ActuallyUseMiningTool orig,
+         Player player,
+         Item sItem,
+         out bool canHitWalls,
+         int treeX,
+         int treeY
+      ) {
+         bool isTree = false;
+         canHitWalls = true;
+
+         // Check if the player is holding an axe
+         if (player.HeldItem.axe <= 0) return isTree;
 
          Tile treeTile = Main.tile[treeX, treeY];
 
+         if (treeTile.TileType == TileID.Trees) isTree = true;
+         else return isTree;
+
          // Get the bottom of the tree
          WorldGen.GetTreeBottom(treeX, treeY, out int groundX, out int groundY);
-         Tile groundTile = Main.tile[groundX, groundY];
 
          // Prevent breaking saplings if the timer is active
-         if (saplingBreakDisableTimer > DateTime.Now && treeTile.TileType == TileID.Saplings) {
+         if (plantBreakDisableTimer > DateTime.Now && treeTile.TileType == TileID.Saplings) {
             canHitWalls = false;
-            return;
+            return isTree;
          }
 
-         // Check if the tile is not the bottom of the tree or the tile above it is not solid
+         // Check if the tile is not the bottom of the tree or the tile above is not a tree
          if (
             treeX != groundX || treeY != groundY - 1 ||
-            Main.tile[treeX, treeY - 1].BlockType != BlockType.Solid
+            Main.tile[treeX, treeY - 1].TileType != TileID.Trees
          ) {
-            orig.Invoke(player, sItem, out canHitWalls, treeX, treeY);
-            return;
+            return isTree;
          }
 
          orig.Invoke(player, sItem, out canHitWalls, treeX, treeY);
 
          // Return if the tree was not broken
-         if (treeTile.TileType != TileID.Dirt) return;
+         if (treeTile.TileType != TileID.Dirt) return true;
 
          // Place a sapling at the tree's position
-         WorldGen.PlaceTile(treeX, treeY, TileID.Saplings);
+         if (!TileObject.CanPlace(
+            treeX,
+            treeY,
+            TileID.Saplings,
+            treeTile.TileFrameX / 18,
+            player.direction,
+            out TileObject tileData
+         )) return isTree;
+         bool placed = TileObject.Place(tileData);
 
          // If a sapling was placed, set the timer to disable breaking it
-         if (treeTile.TileType == TileID.Saplings) {
-            saplingBreakDisableTimer = DateTime.Now.AddSeconds(saplingBreakDisableTime);
+         if (placed) {
+            plantBreakDisableTimer = DateTime.Now.AddSeconds(plantBreakDisableTime);
          }
+
+         isTree = true;
+         return isTree;
+      }
+
+      private bool BreakHerb(
+         On_Player.orig_ItemCheck_UseMiningTools_ActuallyUseMiningTool orig,
+         Player player,
+         Item sItem,
+         out bool canHitWalls,
+         int x,
+         int y
+      ) {
+         bool isHerb = false;
+         canHitWalls = true;
+
+         // Check if the player is holding a pickaxe
+         if (player.HeldItem.pick <= 0) return isHerb;
+
+         // Get the ReplantHerbs config value from Regrowth Replant
+         bool ReplantHerbs = GetRegrowReplantConfig("ReplantHerbs");
+
+         // Check if Regrowth Replant is enabled
+         if (!ReplantHerbs) return isHerb;
+
+         Tile tile = Main.tile[x, y];
+
+         // Prevent breaking herbs if the timer is active
+         if (plantBreakDisableTimer > DateTime.Now && tile.TileType == TileID.ImmatureHerbs) {
+            canHitWalls = false;
+            return isHerb;
+         }
+
+         // Check if the tile is not a herb
+         if (
+            tile.TileType != TileID.ImmatureHerbs &&
+            tile.TileType != TileID.MatureHerbs &&
+            tile.TileType != TileID.BloomingHerbs
+         ) return isHerb;
+
+         int herbStyle = tile.TileFrameX / 18;
+
+         orig.Invoke(player, sItem, out canHitWalls, x, y);
+
+         // Return if the herb was not broken
+         if (tile.TileType != TileID.Dirt) return isHerb;
+
+         // Place a herb at the tree's position
+         WorldGen.PlaceTile(x, y, TileID.ImmatureHerbs, style: herbStyle);
+         if (!TileObject.CanPlace(
+            x,
+            y,
+            TileID.ImmatureHerbs,
+            herbStyle,
+            player.direction,
+            out TileObject objectData
+         )) return isHerb;
+         bool placed = TileObject.Place(objectData);
+
+         // If a herb was placed, set the timer to disable breaking it
+         if (placed) {
+            plantBreakDisableTimer = DateTime.Now.AddSeconds(plantBreakDisableTime);
+         }
+
+         isHerb = true;
+         return isHerb;
+      }
+
+      private bool BreakGemcorn(
+         On_Player.orig_ItemCheck_UseMiningTools_ActuallyUseMiningTool orig,
+         Player player,
+         Item sItem,
+         out bool canHitWalls,
+         int gemcornX,
+         int gemcornY
+      ) {
+         bool isGemcorn = false;
+         canHitWalls = true;
+
+         // Check if the player is holding an axe
+         if (player.HeldItem.axe <= 0) return isGemcorn;
+
+         // Get the ReplantGemcorn config value from Regrowth Replant
+         bool ReplantGemcorn = GetRegrowReplantConfig("ReplantGemcorns");
+
+         // Check if Regrowth Replant is enabled
+         if (!ReplantGemcorn) return isGemcorn;
+
+         Tile gemcornTile = Main.tile[gemcornX, gemcornY];
+
+         // Check if the tile is not a gemcorn
+         if (Array.Exists(gemcornTrees, style => gemcornTile.TileType == style)) isGemcorn = true;
+         else return isGemcorn;
+
+         WorldGen.GetTreeBottom(gemcornX, gemcornY, out int groundX, out int groundY);
+
+         // Prevent breaking gemcorns if the timer is active
+         if (plantBreakDisableTimer > DateTime.Now && gemcornTile.TileType == TileID.GemSaplings) {
+            canHitWalls = false;
+            return isGemcorn;
+         }
+
+         // Check if the tile is not the bottom of the tree or the tile above is not a gemcorn
+         if (
+            gemcornX != groundX || gemcornY != groundY - 1 ||
+            !Array.Exists(gemcornTrees, style => Main.tile[gemcornX, gemcornY - 1].TileType == style)
+         ) {
+            return isGemcorn;
+         }
+
+         ushort gemcornType = (ushort)(gemcornTile.TileType - TileID.TreeTopaz);
+
+         orig.Invoke(player, sItem, out canHitWalls, gemcornX, gemcornY);
+
+         // Return if the gemcorn was not broken
+         if (gemcornTile.TileType != TileID.Dirt) return true;
+
+         // Place a gemcorn at the tree's position
+         if (!TileObject.CanPlace(
+            gemcornX,
+            gemcornY,
+            TileID.GemSaplings,
+            gemcornType,
+            player.direction,
+            out TileObject objectData
+         )) return isGemcorn;
+         bool placed = TileObject.Place(objectData);
+
+         // If a gemcorn was placed, set the timer to disable breaking it
+         if (placed) {
+            plantBreakDisableTimer = DateTime.Now.AddSeconds(plantBreakDisableTime);
+         }
+
+         isGemcorn = true;
+         return isGemcorn;
+      }
+
+      private bool GetRegrowReplantConfig(string configName) {
+         FieldInfo configFieldInfo = regrowthReplant.Code.GetType("RegrowthReplant.RegrowthReplantServerConfig").GetField(configName);
+         return (bool)configFieldInfo.GetValue(regrowthReplant.GetConfig("RegrowthReplantServerConfig"));
       }
    }
 }
